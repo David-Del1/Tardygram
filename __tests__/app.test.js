a260a077dd8403302129c1117fd871b0911d2942
David Delgadillo <daviddel.731@gmail.com>
2021-07-01 09:18:54 -0700

    completed POST and DELETE route for comment. tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ import request from 'supertest';
 import app from '../lib/app.js';
 import UserService from '../lib/services/UserService.js';
 import Post from '../lib/models/Post.js';
+import Comment from '../lib/models/Comment.js';
 
 const agent = request.agent(app);
 
@@ -143,6 +144,99 @@ describe('Post tests', () => {
 
     expect(res.body).toEqual(post);
   });
+
+  it('deletes a post by id via DELETE', async () => {
+    const post = await Post.insert({
+      userId: user.id,
+      photoUrl: 'blah',
+      caption: 'Look at this cool post!',
+      tags: ['cool', 'amaz-ing', 'awesome']
+    });
+
+    const res = await agent
+      .delete(`/api/v1/posts/${post.id}`)
+      .send(post);
+
+    expect(res.body).toEqual(post);
+  });
+
+  // it('gets most popular posts via GET', async () => {
+
+  // })
   
 });
+
+describe('Comment routes', () => {
+
+  let user = {};
+  
+  let agent;
+
+  beforeEach(async() => {
+    await setup(pool);
+    agent = request.agent(app);
+    user = await UserService.create({
+      username: 'datboi456',
+      password: 'wordpass',
+      profilePhotoUrl: 'me.jpg'
+    });
+    await agent.post('/api/v1/auth/login')
+      .send({
+        username: 'datboi456',
+        password: 'wordpass',
+      });
+  });
+
+  it('creates a comment via POST', async () => {
+    
+    const post = await Post.insert({
+      userId: user.id,
+      photoUrl: 'blah',
+      caption: 'Look at this cool post!',
+      tags: ['cool', 'amaz-ing', 'awesome']
+
+    });
+
+    const res = await agent
+      .post('/api/v1/comments')
+      .send({
+        commentBy: user.id,
+        post: post.id,
+        comment: 'Looks uber cool'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      commentBy: user.id,
+      post: post.id,
+      comment: 'Looks uber cool'
+    });
+  });
+
+  it('deletes a comment by id via DELETE', async () => {
+    const post = await Post.insert({
+      userId: user.id,
+      photoUrl: 'blah',
+      caption: 'Look at this cool post!',
+      tags: ['cool', 'amaz-ing', 'awesome']
+
+    });
+
+    const comment = await Comment.insert({
+      commentBy: user.id,
+      post: post.id,
+      comment: 'Looks uber cool'
+    });
+
+    const res = await agent
+      .delete(`/api/v1/comments/${comment.id}`)
+      .send(comment);
+
+    expect(res.body).toEqual(comment);
+
+
+  });
+  
+});
+
 
